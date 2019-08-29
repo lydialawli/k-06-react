e4aa25ed223818c2204ddd6ce357051bbdd17902
Ly <lydialawli@gmail.com>
2019-08-29 12:35:20 +0700

    060302

---
+++ 060101/src/components/Favorites.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../styles/Favorites.css'
+
+
+
+class Favorites extends React.Component {
+
+    render() {
+        return <div class="favorites"></div>
+    }
+
+}
+
+
+export default Favorites
