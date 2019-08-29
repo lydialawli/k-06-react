df593c0a6a54797152a57f1c3352dbffad1236c0
Ly <lydialawli@gmail.com>
2019-08-29 12:27:05 +0700

    060203 format spacing

--- 060101/src/components/Places.js
+++ 060101/src/components/Places.js
@@ -46,9 +46,9 @@ class Places extends React.Component {
                         this.state.places.map(p => {
                             return (
                                 <div class="thumbnail">
-                                <h2>{p.title}</h2>
-                                <h3>{p.price}</h3>
-                                <p>{p.location}</p>
+                                    <h2>{p.title}</h2>
+                                    <h3>{p.price}</h3>
+                                    <p>{p.location}</p>
                                 </div>
                             )
                         })
