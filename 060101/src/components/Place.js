e07b657319d0f03d89ba330cef44bdd8971d00c4
Ly <lydialawli@gmail.com>
2019-09-02 11:20:55 +0700

    060802

---
+++ 060101/src/components/Place.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+class Place extends React.Component {
+
+    render() {
+        return <h1>I'm a simple text!</h1>
+    }
+
+}
+
+
+export default Place
--- 060101/src/components/Routes.js
+++ 060101/src/components/Routes.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Places from './Places'
+import Place from './Place'
 
 //make sure larger paths goes first
 class Routes extends React.Component {
@@ -8,6 +9,7 @@ class Routes extends React.Component {
         return (
             <BrowserRouter>
                 <Switch>
+                    <Route path='/place' component={Place}></Route>
                     <Route path='/' component={Places}></Route>
                 </Switch>
             </BrowserRouter>
